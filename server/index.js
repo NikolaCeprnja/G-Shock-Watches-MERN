45882eb876fb71339000492285d701161e2ec252
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2020-11-10 22:06:01 +0100

    Setup basic server and connect to MongoDB Atlas

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const env = require('dotenv');
+const mongoose = require('mongoose');
+
+if (process.env.NODE_ENV !== 'production') env.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+mongoose
+	.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		app.listen(port, error => {
+			if (error) throw new Error(err);
+
+			console.log(`Server is running on port: ${port}.`);
+		});
+	})
+	.catch(error => {
+		console.error(error);
+	});

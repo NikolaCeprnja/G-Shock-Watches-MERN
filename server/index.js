dfe6054632c9da4f69fa515ffa09f445a46a55fd
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2020-11-30 23:53:28 +0100

    Configure User routes and setup default error handling

--- server/index.js
+++ server/index.js
@@ -4,13 +4,36 @@ const cors = require('cors');
 const env = require('dotenv');
 const mongoose = require('mongoose');
 
+const userRoutes = require('./routes/users-routes');
+
 if (process.env.NODE_ENV !== 'production') env.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/users', userRoutes);
+
+app.use((req, res, next) => {
+	res.status(404).json({ message: 'Error, this page does not exists.' });
+});
+
+app.use((err, req, res, next) => {
+	console.error(err);
+
+	if (res.headersSent) {
+		return next(err);
+	}
+
+	return res.status(err.statusCode || 500).json({
+		message:
+			err.message ||
+			'Server error, something went wrong, please try again later.',
+		error: err,
+	});
+});
 
 mongoose
 	.connect(process.env.DB_URI, {

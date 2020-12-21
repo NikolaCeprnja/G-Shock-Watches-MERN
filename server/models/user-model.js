4a1fbd781a792399b26df5d2ce423f712ddf1880
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2020-12-22 00:44:05 +0100

    Add bcryptjs and pre-save validation for unique fields

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,14 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const mongooseUniqueValidator = require('mongoose-unique-validator')
 
-const { Schema } = mongoose;
+const { Schema } = mongoose
 
 const userSchema = new Schema({
-	userName: { type: String, required: true },
-	email: { type: String, required: true },
-	password: { type: String, required: true },
-	isAdmin: { type: Boolean, default: false },
-	purchasedProducts: [{ type: mongoose.Types.ObjectId, ref: 'Product' }],
-	reviews: [{ type: mongoose.Types.ObjectId, ref: 'Review' }],
-});
+  userName: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
+  purchasedProducts: [{ type: mongoose.Types.ObjectId, ref: 'Product' }],
+  reviews: [{ type: mongoose.Types.ObjectId, ref: 'Review' }],
+})
 
-module.exports = mongoose.model('Users', userSchema);
+// pre-save hook for hasing password with bcryptjs
+userSchema.pre('save', async function (next) {
+  const user = this
+  const hashPassword = await bcrypt.hash(user.password, 12)
+
+  user.password = hashPassword
+  return next()
+})
+
+// custom method for validating hashed password
+userSchema.methods.isValidPassword = async function (password) {
+  const user = this
+  const compare = await bcrypt.compare(password, user.password)
+
+  return compare
+}
+
+// plugin which adds pre-save validation for unique fields in userSchema
+userSchema.plugin(mongooseUniqueValidator)
+
+module.exports = mongoose.model('Users', userSchema)

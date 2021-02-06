3f4dec1998b6a829786e41285967c157a940d38c
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2021-02-07 00:37:02 +0100

    Add timestamps, remove pre save and add pre deleteOne hook for userSchema

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,26 +1,53 @@
+/* eslint-disable global-require */
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const mongooseUniqueValidator = require('mongoose-unique-validator')
 
+const ErrorHandler = require('./error-handler')
+
 const { Schema } = mongoose
 
-const userSchema = new Schema({
-  userName: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  isAdmin: { type: Boolean, default: false },
-  purchasedProducts: [{ type: mongoose.Types.ObjectId, ref: 'Product' }],
-  reviews: [{ type: mongoose.Types.ObjectId, ref: 'Review' }],
-})
+const userSchema = new Schema(
+  {
+    userName: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+    purchasedProducts: [{ type: mongoose.Types.ObjectId, ref: 'Product' }],
+    reviews: [{ type: mongoose.Types.ObjectId, ref: 'Review' }],
+  },
+  { timestamps: true }
+)
 
-// pre-save hook for hasing password with bcryptjs
-userSchema.pre('save', async function (next) {
-  const user = this
-  const hashPassword = await bcrypt.hash(user.password, 12)
+// pre-deleteOne method that will delete all references for user and product reviews before deleting a user account
+userSchema.pre(
+  'deleteOne',
+  { document: true, query: false },
+  async function (next) {
+    const Review = require('./review-model')
+    const Product = require('./product-model')
+    const user = this
 
-  user.password = hashPassword
-  return next()
-})
+    try {
+      const session = await mongoose.startSession()
+      session.startTransaction()
+      await Product.updateMany(
+        { _id: { $in: user.purchasedProducts } },
+        {
+          $pullAll: { reviews: user.reviews },
+        }
+      ).session(session)
+      await Review.deleteMany({ _id: { $in: user.reviews } }).session(session)
+      await session.commitTransaction()
+      session.endSession()
+    } catch (err) {
+      console.log(err)
+      return next(
+        new ErrorHandler('Something went wrong, please try again later.', 500)
+      )
+    }
+  }
+)
 
 // custom method for validating hashed password
 userSchema.methods.isValidPassword = async function (password) {

b8a8389518be35d1a67a8f3027f3967cd2dd4050
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2021-02-07 00:38:51 +0100

    Add timestamps, and pre deleteOne hook for productSchema

--- server/models/product-model.js
+++ server/models/product-model.js
@@ -1,23 +1,61 @@
+/* eslint-disable global-require */
 const mongoose = require('mongoose')
 const Double = require('@mongoosejs/double/lib')
 
+const ErrorHandler = require('./error-handler')
+
 const { Schema } = mongoose
 
-const productSchema = new Schema({
-  name: { type: String, required: true },
-  model: { type: String, required: true },
-  color: { type: String, required: true },
-  types: [{ type: String, required: true }],
-  collectionName: { type: String, required: true },
-  materials: [{ type: String, required: true }],
-  functions: [{ type: String, required: true }],
-  images: [{ type: String, required: true }],
-  discount: { type: Number, default: 0 },
-  inStock: { type: Number, required: true },
-  reviews: [{ type: mongoose.Types.ObjectId, ref: 'Review' }],
-  specifications: [{ type: String, required: true }],
-  price: { type: Double, required: true },
-  created: { type: Date, required: true },
-})
+const productSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    model: { type: String, required: true },
+    color: { type: String, required: true },
+    types: [{ type: String, required: true }],
+    collectionName: { type: String, required: true },
+    materials: [{ type: String, required: true }],
+    functions: [{ type: String, required: true }],
+    images: [{ type: String, required: true }],
+    discount: { type: Number, default: 0 },
+    inStock: { type: Number, required: true },
+    reviews: [{ type: mongoose.Types.ObjectId, ref: 'Review' }],
+    specifications: [{ type: String, required: true }],
+    price: { type: Double, required: true },
+  },
+  { timestamps: true }
+)
+
+productSchema.pre(
+  'deleteOne',
+  { document: true, query: false },
+  async function (next) {
+    const User = require('./user-model')
+    const Review = require('./review-model')
+    const product = this
+
+    try {
+      const session = await mongoose.startSession()
+      session.startTransaction()
+      await User.updateMany(
+        { purchasedProducts: product._id },
+        {
+          $pull: {
+            purchasedProducts: product._id,
+            reviews: { $in: product.reviews },
+          },
+        }
+      ).session(session)
+      await Review.deleteMany({ _id: { $in: product.reviews } }).session(
+        session
+      )
+      await session.commitTransaction()
+      session.endSession()
+    } catch (err) {
+      return next(
+        new ErrorHandler('Something went wrong, please try again later.', 500)
+      )
+    }
+  }
+)
 
 module.exports = mongoose.model('Product', productSchema)

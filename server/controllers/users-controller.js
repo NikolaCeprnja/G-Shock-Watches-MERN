6ff334b1c946c9abb4076096c5701c23474dc41e
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2021-02-07 01:24:28 +0100

    Remove auth middlewares from user controllers and add delete route

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -1,15 +1,9 @@
-const { validationResult } = require('express-validator')
-
 const User = require('../models/user-model')
 const ErrorHandler = require('../models/error-handler')
-const { auth } = require('../auth/auth')
 
 // GET CONTROLLERS
 const getUsers = async (req, res, next) => {
   let users
-
-  if (!req.user.isAdmin)
-    return next(new ErrorHandler("You don't have permission to do that.", 403))
 
   try {
     users = await User.find()
@@ -30,10 +24,6 @@ const getUserById = async (req, res, next) => {
   const { uid } = req.params
   let user
 
-  if (req.user.id !== uid && !req.user.isAdmin) {
-    return next(new ErrorHandler("You don't have permission to do that.", 403))
-  }
-
   try {
     user = await User.findById(uid)
   } catch (error) {
@@ -51,36 +41,46 @@ const getUserById = async (req, res, next) => {
     .json({ message: 'User with provided uid does not exists.' })
 }
 
-// POST CONTROLLERS
-const userSignup = async (req, res, next) => {
-  const valErr = validationResult(req)
+// DELETE CONTROLLERS
+const deleteUser = async (req, res, next) => {
+  const { uid } = req.params
+  let user
 
-  if (!valErr.isEmpty()) {
-    console.error(valErr)
+  try {
+    user = await User.findById(uid)
+  } catch (err) {
     return next(
-      new ErrorHandler('Invalid inputs passed, please check your data.', 422)
+      new ErrorHandler('Something went wrong, please try again later.', 500)
     )
   }
 
-  auth('signup', req, res, next)
-}
+  if (user) {
+    try {
+      await user.deleteOne()
+    } catch (err) {
+      return next(
+        new ErrorHandler('Something went wrong, please try again later.', 500)
+      )
+    }
 
-const userSignin = async (req, res, next) => {
-  const valErr = validationResult(req)
+    if (req.user.id === uid) {
+      req.logout()
+      res.clearCookie('token', { httpOnly: true })
+    }
 
-  if (!valErr.isEmpty()) {
-    console.error(valErr)
-    return next(
-      new ErrorHandler('Invalid inputs passed, please check your data.', 422)
-    )
+    return res.json({
+      message: 'User is successfully deleted!',
+      deletedUser: user.toObject({ getters: true }),
+    })
   }
 
-  auth('signin', req, res, next)
+  return res
+    .status(404)
+    .json({ message: 'User with provided uid does not exists.' })
 }
 
 module.exports = {
   getUsers,
   getUserById,
-  userSignup,
-  userSignin,
+  deleteUser,
 }

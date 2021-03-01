62f66be3b1dab35b312c70621992ac4cb330e25b
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2021-03-01 19:33:02 +0100

    Initial CRA with redux template, folder structure and dependencies

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './App.less'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>G-Shock-Watches</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App

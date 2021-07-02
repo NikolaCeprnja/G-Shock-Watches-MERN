7ee6920c1df196265a57d84b1a89e8535f597aac
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2021-07-03 00:31:42 +0200

    Add additional attributes to img tag

--- packages/client/src/components/CollectionItem/index.jsx
+++ packages/client/src/components/CollectionItem/index.jsx
@@ -10,7 +10,14 @@ const CollectionItem = ({ name, imgUrl, ...otherProps }) => {
   return (
     <Item className='collection-item' {...otherProps}>
       <div>
-        <img className='collection-img' src={imgUrl} alt={`${name}-watch`} />
+        <img
+          className='collection-img'
+          src={imgUrl}
+          alt={`${name}-watch`}
+          width='200'
+          height='200'
+          loading='lazy'
+        />
         <h4 className='collection-name'>{name}</h4>
       </div>
     </Item>

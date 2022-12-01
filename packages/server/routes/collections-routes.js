e56c40b274dee620c6d66b4b7c5a72b4ae6c0e9b
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2022-12-01 19:49:15 +0100

    Update collection model and add getCollections controller and route endpoint.

--- packages/server/routes/collections-routes.js
+++ packages/server/routes/collections-routes.js
@@ -1,9 +1,15 @@
 const router = require('express').Router()
 
 const {
+  getCollections,
   getCollectionsByGender,
 } = require('../controllers/collections-controller')
 
+// GET ROUTES
+/** @method GET @access PUBLIC @desc Get all collections. */
+router.get('/', getCollections)
+
+/** @method GET @access PUBLIC @desc Get collections by gender type. */
 router.get('/:gender', getCollectionsByGender)
 
 module.exports = router

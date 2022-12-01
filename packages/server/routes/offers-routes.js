9009f2642932e3c91b12cbf21423cdc4bfd7ef9e
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2022-12-01 19:56:20 +0100

    Add JSDoc comment for offer route endpoint.

--- packages/server/routes/offers-routes.js
+++ packages/server/routes/offers-routes.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 
 const { getOffers } = require('../controllers/offers-contoller')
 
+// GET ROUTES
+/** @method GET @access PUBLIC @desc Get all offers. */
 router.get('/', getOffers)
 
 module.exports = router

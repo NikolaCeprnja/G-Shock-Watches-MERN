9c63cb6d43eb585c596d0abdd79f759e59c48b14
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2022-12-04 00:16:15 +0100

    Add multer middleware for createProduct route and add JSDoc comments for all product routes.

--- packages/server/routes/products-routes.js
+++ packages/server/routes/products-routes.js
@@ -1,4 +1,7 @@
 const router = require('express').Router()
+const multer = require('multer')
+
+const upload = multer()
 
 const {
   getProducts,
@@ -17,29 +20,39 @@ const { isAdmin } = require('../middlewares/auth-middleware')
 const { checkReqParamValidity } = require('../middlewares/req-param-middleware')
 
 // GET ROUTES
+/** @method GET @access PUBLIC @desc Get all products. */
 router.get('/', getProducts)
 
+/** @method GET @access PUBLIC @desc Get latest products. */
 router.get('/latest', getLatestProducts)
 
+/** @method GET @access PUBLIC @desc Get top rated products. */
 router.get('/top-rated', getTopRatedProducts)
 
+/** @method GET @access PUBLIC @desc Get product by its pid. */
 router.get('/:pid', checkReqParamValidity('pid'), getProductById)
 
 // POST ROUTES
+/** @method POST @access PRIVATE @desc Create new product. */
 router.post(
   '/create',
   authJwt,
   isAdmin,
+  upload.fields([
+    { name: 'images[]', maxCount: 5 },
+    { name: 'previewImg', maxCount: 1 },
+  ]),
   productValidation('createProduct'),
   createProduct
 )
 
 // DELETE ROUTES
+/** @method DELETE @access PRIVATE @desc Delete product by its pid. */
 router.delete(
   '/:pid',
   authJwt,
-  checkReqParamValidity('pid'),
   isAdmin,
+  checkReqParamValidity('pid'),
   deleteProduct
 )
 

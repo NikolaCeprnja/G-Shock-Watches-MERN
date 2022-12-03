35dd5b8e7722633e8fcff5b9eff2682b0f13d27b
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2022-12-03 23:40:46 +0100

    Add pagination and url query support and modify mongoose query to use aggregation pipeline for getProducts and getProductById middlewares.

--- packages/server/controllers/products-controller.js
+++ packages/server/controllers/products-controller.js
@@ -7,22 +7,177 @@ const ErrorHandler = require('../models/error-handler')
 // GET CONTROLLERS
 const getProducts = async (req, res, next) => {
   let products
+  const showPerPage = Number(req.query.pageSize) || 8
+  if (req.query.pageSize) delete req.query.pageSize
+  const curPage = (({ page }) => Number(page))(req.query) || 1
+  if (req.query.page) delete req.query.page
+  const skipNext = showPerPage * (curPage - 1)
+  let searchQuery
+  if (Object.keys(req.query).includes('searchTerm')) {
+    searchQuery = {
+      $text: {
+        $search: req.query.searchTerm,
+        $language: 'none',
+        $diacriticSensitive: true,
+      },
+    }
+  }
+  const collectionQuery = {}
+  if (req.query.collectionName) {
+    collectionQuery.name = {
+      $in: Array.isArray(req.query.collectionName)
+        ? req.query.collectionName
+        : [req.query.collectionName],
+    }
+  }
+  if (req.query.gender) {
+    collectionQuery.gender = req.query.gender
+  }
+  const totalProducts = await Collection.aggregate([
+    { $match: { ...collectionQuery } },
+    {
+      $lookup: {
+        from: 'products',
+        let: { products: '$products' },
+        pipeline: [
+          {
+            $match: {
+              $expr: { $in: ['$_id', '$$products'] },
+              ...searchQuery,
+            },
+          },
+          { $count: 'count' },
+        ],
+        as: 'products',
+      },
+    },
+    { $unwind: '$products' },
+    { $group: { _id: null, count: { $sum: '$products.count' } } },
+  ])
+  let sortBy
+
+  if (req.query.sortBy) {
+    sortBy = JSON.parse(req.query.sortBy)
+
+    if (Object.keys(sortBy)[0] === 'numReviews') {
+      sortBy = {
+        'data.avgRating': Object.values(sortBy)[0] === 'ascend' ? 1 : -1,
+        [`data.${Object.keys(sortBy)[0]}`]:
+          Object.values(sortBy)[0] === 'ascend' ? 1 : -1,
+      }
+    } else {
+      sortBy = {
+        [`data.${Object.keys(sortBy)[0]}`]:
+          Object.values(sortBy)[0] === 'ascend' ? 1 : -1,
+      }
+    }
+  }
 
   try {
-    products = await Product.find()
+    products = await Collection.aggregate([
+      { $match: { ...collectionQuery } },
+      {
+        $lookup: {
+          from: 'products',
+          let: { products: '$products', gender: '$gender' },
+          pipeline: [
+            {
+              $match: {
+                $expr: { $in: ['$_id', '$$products'] },
+                ...searchQuery,
+              },
+            },
+            {
+              $lookup: {
+                from: 'reviews',
+                localField: 'reviews',
+                foreignField: '_id',
+                as: 'reviews',
+              },
+            },
+            {
+              $project: {
+                _id: 0,
+                id: '$_id',
+                name: 1,
+                model: 1,
+                gender: '$$gender',
+                collectionName: 1,
+                previewImg: 1,
+                discount: {
+                  $cond: {
+                    if: { $ne: ['$discount', 0] },
+                    then: '$discount',
+                    else: '$$REMOVE',
+                  },
+                },
+                inStock: 1,
+                price: 1,
+                numReviews: {
+                  $cond: {
+                    if: { $ne: [{ $size: '$reviews' }, 0] },
+                    then: { $size: '$reviews' },
+                    else: '$$REMOVE',
+                  },
+                },
+                avgRating: {
+                  $cond: {
+                    if: { $eq: ['$reviews', []] },
+                    then: '$$REMOVE',
+                    else: { $avg: '$reviews.score' },
+                  },
+                },
+                createdAt: {
+                  $dateToString: {
+                    format: '%d/%m/%Y',
+                    date: '$createdAt',
+                  },
+                },
+              },
+            },
+          ],
+          as: 'data',
+        },
+      },
+      { $unwind: '$data' },
+      { $skip: skipNext },
+      { $limit: showPerPage },
+      {
+        $sort: sortBy || { 'data.createdAt': -1, 'data.id': -1 },
+      },
+      {
+        $group: {
+          _id: null,
+          data: { $push: '$data' },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          data: 1,
+        },
+      },
+    ])
   } catch (err) {
     return next(
       new ErrorHandler('Something went wrong, please try again later.', 500)
     )
   }
 
-  if (products.length) {
-    return res.status(200).json({ message: 'List of all products', products })
+  if (products.length > 0) {
+    return res.status(200).json({
+      message: 'List of all products',
+      curPage,
+      pageSize: showPerPage,
+      products: products[0].data,
+      totalProducts: totalProducts[0].count,
+    })
   }
 
-  return res
-    .status(404)
-    .json({ message: 'Currently there is no products yet.' })
+  return res.status(404).json({
+    message:
+      'No items were found matching this combination of selected filters.',
+  })
 }
 
 const getLatestProducts = async (req, res, next) => {
@@ -157,15 +312,148 @@ const getProductById = async (req, res, next) => {
   let product
 
   try {
-    product = await Product.findById(pid)
+    product = await Product.aggregate([
+      { $match: { _id: mongoose.Types.ObjectId(pid) } },
+      {
+        $lookup: {
+          from: 'collections',
+          localField: 'collectionName',
+          foreignField: 'name',
+          as: 'collection',
+        },
+      },
+      {
+        $lookup: {
+          from: 'reviews',
+          let: { reviews: '$reviews' },
+          pipeline: [
+            {
+              $match: { $expr: { $in: ['$_id', '$$reviews'] } },
+            },
+            { $sort: { createdAt: -1 } },
+            // { $limit: 1 },
+            {
+              $lookup: {
+                from: 'users',
+                localField: 'creator',
+                foreignField: '_id',
+                as: 'creator',
+              },
+            },
+          ],
+          as: 'reviews',
+        },
+      },
+      {
+        $lookup: {
+          from: 'products',
+          let: { id: '$_id', name: '$name' },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    { $ne: ['$_id', '$$id'] },
+                    { $eq: ['$name', '$$name'] },
+                  ],
+                },
+              },
+            },
+            {
+              $project: {
+                _id: 0,
+                id: '$_id',
+                name: 1,
+                model: 1,
+                previewImg: 1,
+              },
+            },
+          ],
+          as: 'otherColors',
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          id: '$_id',
+          name: 1,
+          model: 1,
+          gender: { $arrayElemAt: ['$collection.gender', 0] },
+          desc: 1,
+          color: 1,
+          types: 1,
+          collectionName: 1,
+          materials: 1,
+          mainFeatures: 1,
+          specifications: 1,
+          previewImg: 1,
+          images: 1,
+          discount: {
+            $cond: {
+              if: { $ne: ['$discount', 0] },
+              then: '$discount',
+              else: '$$REMOVE',
+            },
+          },
+          inStock: 1,
+          reviews: {
+            $map: {
+              input: '$reviews',
+              as: 'review',
+              in: {
+                id: '$$review._id',
+                title: '$$review.title',
+                desc: '$$review.description',
+                score: '$$review.score',
+                createdAt: {
+                  $dateToString: {
+                    format: '%d/%m/%Y',
+                    date: '$$review.createdAt',
+                  },
+                },
+                creator: {
+                  id: { $arrayElemAt: ['$$review.creator._id', 0] },
+                  userName: { $arrayElemAt: ['$$review.creator.userName', 0] },
+                  avatarUrl: {
+                    $arrayElemAt: ['$$review.creator.avatarUrl', 0],
+                  },
+                },
+              },
+            },
+          },
+          price: 1,
+          otherColors: {
+            $cond: {
+              if: { $eq: ['$otherColors', []] },
+              then: '$$REMOVE',
+              else: '$otherColors',
+            },
+          },
+          numReviews: {
+            $cond: {
+              if: { $ne: [{ $size: '$reviews' }, 0] },
+              then: { $size: '$reviews' },
+              else: '$$REMOVE',
+            },
+          },
+          avgRating: {
+            $cond: {
+              if: { $eq: ['$reviews', []] },
+              then: '$$REMOVE',
+              else: { $avg: '$reviews.score' },
+            },
+          },
+        },
+      },
+    ])
   } catch (err) {
     return next(
       new ErrorHandler('Something went wrong, please try again later.', 500)
     )
   }
 
-  if (product) {
-    return res.status(200).json({ product: product.toObject() })
+  if (product[0]) {
+    return res.status(200).json({ product: product[0] })
   }
 
   return res

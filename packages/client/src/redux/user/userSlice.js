079ca8c82bcf7261eba2f7d4894779db16686346
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2022-12-07 15:09:15 +0100

    Move user async thunks into separate file, update userSlice reducers and export user state selectors and actions.

--- packages/client/src/redux/user/userSlice.js
+++ packages/client/src/redux/user/userSlice.js
@@ -1,138 +1,201 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+/* eslint-disable no-unused-vars */
+import { createSlice, createDraftSafeSelector } from '@reduxjs/toolkit'
 
-import auth from '@api/user/auth'
-
-// Thunks
-export const signin = createAsyncThunk(
-  'user/signin',
-  async (userData, { rejectWithValue }) => {
-    try {
-      const response = await auth.signin(JSON.stringify(userData))
-      const { data, status } = response
-      return { ...data, status }
-    } catch (err) {
-      const { data, status, statusText } = err.response
-      return rejectWithValue({
-        data,
-        status,
-        statusText,
-      })
-    }
-  }
-)
-
-export const signup = createAsyncThunk(
-  'user/signup',
-  async (userData, { rejectWithValue }) => {
-    try {
-      const response = await auth.signup(userData)
-      const { data, status } = response
-      return { ...data, status }
-    } catch (err) {
-      const { data, status, statusText } = err.response
-      return rejectWithValue({
-        data,
-        status,
-        statusText,
-      })
-    }
-  }
-)
-
-export const signout = createAsyncThunk(
-  'user/signout',
-  async (user, { rejectWithValue }) => {
-    try {
-      const response = await auth.signout()
-      const { data, status } = response
-      return { ...data, status }
-    } catch (err) {
-      const { data, status, statusText } = err.response
-      return rejectWithValue({
-        data,
-        status,
-        statusText,
-      })
-    }
-  }
-)
-
-export const authUser = createAsyncThunk(
-  'user/auth',
-  async (user, { rejectWithValue }) => {
-    try {
-      const response = await auth.authUser()
-      const { data, status } = response
-      return { ...data, status }
-    } catch (err) {
-      const { data, status, statusText } = err.response
-      return rejectWithValue({
-        data,
-        status,
-        statusText,
-      })
-    }
-  }
-)
+import * as userThunk from './userThunk'
 
 const initialState = {
-  auth: 'unauthenticated',
-  loading: false,
-  info: undefined,
+  loggedInUser: {
+    auth: 'unauthenticated',
+    loading: false,
+    updating: false,
+    info: undefined,
+  },
+  all: {
+    loading: false,
+    data: undefined,
+    curPage: undefined,
+    pageSize: undefined,
+    totalData: undefined,
+  },
+  preview: {
+    loading: false,
+    updating: false,
+    info: undefined,
+  },
 }
 
 export const userSlice = createSlice({
-  name: 'user',
+  name: 'users',
   initialState,
-  reducers: {},
-  extraReducers: {
-    [signin.pending]: state => {
-      state.loading = true
+  reducers: {
+    clearUserPreview: ({ preview }) => {
+      preview.info = undefined
     },
-    [signin.fulfilled]: (state, { payload }) => {
-      Object.assign(state, {
+  },
+  extraReducers: {
+    [userThunk.signin.pending]: ({ loggedInUser }) => {
+      loggedInUser.loading = true
+    },
+    [userThunk.signin.fulfilled]: ({ loggedInUser }, { payload }) => {
+      Object.assign(loggedInUser, {
         signedIn: true,
         auth: 'authenticated',
         loading: false,
         info: { ...payload.loggedInUser },
       })
     },
-    [signin.rejected]: state => {
-      state.loading = false
+    [userThunk.signin.rejected]: ({ loggedInUser }) => {
+      loggedInUser.loading = false
     },
-    [signup.pending]: state => {
-      state.loading = true
+    [userThunk.signup.pending]: ({ loggedInUser }) => {
+      loggedInUser.loading = true
     },
-    [signup.fulfilled]: (state, { payload }) => {
-      Object.assign(state, {
+    [userThunk.signup.fulfilled]: ({ loggedInUser }, { payload }) => {
+      Object.assign(loggedInUser, {
         signedIn: true,
         auth: 'authenticated',
         loading: false,
         info: { ...payload.signedUpUser },
       })
     },
-    [signup.rejected]: state => {
-      state.loading = false
+    [userThunk.signup.rejected]: ({ loggedInUser }) => {
+      loggedInUser.loading = false
     },
-    [signout.fulfilled]: () => initialState,
+    [userThunk.signout.fulfilled]: () => initialState,
     /*     [signout.rejected]: state => {
       state.loading = false
     }, */
-    [authUser.pending]: state => {
-      state.auth = 'authenticating'
+    [userThunk.authUser.pending]: ({ loggedInUser }) => {
+      loggedInUser.auth = 'authenticating'
     },
-    [authUser.fulfilled]: (state, { payload }) => {
-      Object.assign(state, {
+    [userThunk.authUser.fulfilled]: ({ loggedInUser }, { payload }) => {
+      Object.assign(loggedInUser, {
         auth: 'authenticated',
         info: { ...payload.loggedInUser },
       })
     },
-    [authUser.rejected]: state => {
-      state.auth = 'unauthenticated'
+    [userThunk.authUser.rejected]: ({ loggedInUser }) => {
+      loggedInUser.auth = 'unauthenticated'
     },
+    [userThunk.getUsers.pending]: ({ all }) => {
+      all.loading = true
+    },
+    [userThunk.getUsers.fulfilled]: (
+      { all },
+      { payload: { users: allUsers, curPage, pageSize, totalUsers } }
+    ) => {
+      all.loading = false
+      all.data = allUsers
+      all.curPage = curPage
+      all.pageSize = pageSize
+      all.totalData = totalUsers
+    },
+    [userThunk.getUsers.rejected]: ({ all }) => {
+      all.loading = false
+      all.data = undefined
+      all.curPage = undefined
+      all.pageSize = undefined
+      all.totalData = undefined
+    },
+    [userThunk.getUserById.pending]: ({ preview }) => {
+      preview.loading = true
+    },
+    [userThunk.getUserById.fulfilled]: ({ preview }, { payload: { user } }) => {
+      preview.loading = false
+      preview.info = user
+    },
+    [userThunk.getUserById.rejected]: ({ preview }) => {
+      preview.loading = false
+    },
+    /* [userThunk.getPurchasedProductsAndReviews.pending]: ( state, { meta: { arg: { updateFor }, }, }) => {}, */
+    [userThunk.getPurchasedProductsAndReviews.fulfilled]: (
+      state,
+      { payload: { purchasedProducts }, meta: { arg } }
+    ) => {
+      const { updateFor } = arg
+      state[updateFor].info.purchasedProducts = purchasedProducts
+    },
+    /* [userThunk.getPurchasedProductsAndReviews.rejected]: ( state, { meta: { arg: { updateFor }, }, }) => {}, */
+    // [userThunk.getUserOrders.pending]: () => {},
+    [userThunk.getUserOrders.fulfilled]: (
+      state,
+      { payload: { orders }, meta: { arg } }
+    ) => {
+      const { updateFor } = arg
+      state[updateFor].info.orders = orders
+    },
+    // [userThunk.getUserOrders.rejected]: () => {},
+    [userThunk.updateUser.pending]: (state, { meta: { arg } }) => {
+      const { updateFor } = arg
+      state[updateFor].updating = true
+    },
+    [userThunk.updateUser.fulfilled]: (
+      state,
+      { payload: { updatedUser }, meta: { arg } }
+    ) => {
+      const { updateFor } = arg
+      Object.assign(state[updateFor], {
+        updating: false,
+        info: { ...state[updateFor].info, ...updatedUser },
+      })
+    },
+    [userThunk.updateUser.rejected]: (state, { meta: { arg } }) => {
+      const { updateFor } = arg
+      state[updateFor].updating = false
+    },
+    // [userThunk.updateReview.pending]: () => {},
+    [userThunk.updateReview.fulfilled]: (
+      state,
+      { payload: { updatedReview }, meta: { arg } }
+    ) => {
+      const { updateFor } = arg
+      const { id, productId, ...restData } = updatedReview
+      const reviewToUpdate = state[updateFor].info.purchasedProducts
+        ?.find(product => product.id === productId)
+        .reviews?.find(review => review.id === id)
+
+      Object.assign(reviewToUpdate, restData)
+    },
+    // [userThunk.updateReview.rejected]: () => {},
+    // [userThunk.deleteReview.pending]: () => {},
+    [userThunk.deleteReview.fulfilled]: (
+      state,
+      { meta: { arg }, payload: { productId, reviewId } }
+    ) => {
+      const { updateFor } = arg
+      const { purchasedProducts } = state[updateFor].info
+      const foundProduct = purchasedProducts?.find(
+        product => product.id === productId
+      )
+      const reviews = foundProduct?.reviews?.filter(
+        review => review.id !== reviewId
+      )
+      foundProduct.reviews = reviews
+    },
+    // [userThunk.deleteReview.rejected]: () => {},
+    // [userThunk.deleteUser.pending]: () => {}
+    [userThunk.deleteUser.fulfilled]: (state, action) => initialState,
+    // [userThunk.deleteUser.rejected]: () => {}
   },
 })
 
-export const selectUser = state => state.user
+const selectUsers = state => state.users
+
+export const selectLoggedInUser = createDraftSafeSelector(
+  [selectUsers],
+  allUsers => allUsers.loggedInUser
+)
+
+export const selectUserPreview = createDraftSafeSelector(
+  [selectUsers],
+  allUsers => allUsers.preview
+)
+
+export const selectAllUsers = createDraftSafeSelector(
+  [selectUsers],
+  allUsers => allUsers.all
+)
+
+export const { clearUserPreview } = userSlice.actions
 
 export default userSlice.reducer

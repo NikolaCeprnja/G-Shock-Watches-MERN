56563d85ab12192be5957a2e0b612aa66e2ff05f
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2022-12-13 02:10:22 +0100

    Update SignupPage component - Code refactoring/splitting signup validation schema into separate file, - Update import declarations for redux user thunks and state selectors, - Extract onSubmit Formik prop into handleSubmit method and - Update styles.

--- packages/client/src/pages/SignupPage/index.jsx
+++ packages/client/src/pages/SignupPage/index.jsx
@@ -1,35 +1,82 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { Link } from 'react-router-dom'
-import { unwrapResult } from '@reduxjs/toolkit'
 import { useDispatch, useSelector } from 'react-redux'
 import { Card, Alert, message as infoMessage } from 'antd'
 import { UserOutlined, MailOutlined, LockOutlined } from '@ant-design/icons'
 import { Formik, Field } from 'formik'
 import { Form, SubmitButton } from 'formik-antd'
-import * as Yup from 'yup'
 
 import InputField from '@components/InputField/index'
 import AvatarUpload from '@components/AvatarUpload/index'
 
-import { selectUser, signup } from '@redux/user/userSlice'
+import { signup } from '@redux/user/userThunk'
+import { selectLoggedInUser } from '@redux/user/userSlice'
 import { create as createNotification } from '@redux/notification/notificationSlice'
+import { signupValidationSchema } from '@validation/user-validation'
 
 import './styles.scss'
 
+const InfoMessage = () => {
+  infoMessage.loading({
+    key: 'info',
+    duration: 0,
+    content: 'Creating your account...',
+  })
+}
+
 const SignupPage = () => {
   const dispatch = useDispatch()
-  const loggedInUser = useSelector(selectUser)
+  const loggedInUser = useSelector(selectLoggedInUser)
 
-  const InfoMessage = () => {
-    infoMessage.loading({
-      key: 'info',
-      duration: 0,
-      content: 'Creating your account...',
-    })
-  }
   const [serverResponse, setServerResponse] = useState({})
   const [existingEmails, setExistingEmails] = useState([])
   const [existingUserNames, setExistingUserNames] = useState([])
+
+  const handleSubmit = useCallback(
+    async (values, { setFieldError }) => {
+      setServerResponse({})
+      try {
+        const data = new FormData()
+        Object.keys(values).forEach(key => data.append(key, values[key]))
+
+        const { message } = await dispatch(signup(data)).unwrap()
+
+        dispatch(
+          createNotification({
+            id: 'signedup',
+            type: 'success',
+            title: 'Your account is successfully created!',
+            description: message,
+          })
+        )
+      } catch (error) {
+        const { errors, message } = error.data
+        const { status, statusText } = error
+
+        if (errors) {
+          Object.keys(errors).forEach(err => {
+            if (err === 'userName') {
+              setExistingUserNames(userNames => [
+                ...userNames,
+                errors[err].value,
+              ])
+            }
+            if (err === 'email') {
+              setExistingEmails(emails => [...emails, errors[err].value])
+            }
+            setFieldError(err, errors[err].message)
+          })
+        } else {
+          setServerResponse({
+            status,
+            statusText,
+            message,
+          })
+        }
+      }
+    },
+    [dispatch]
+  )
 
   useEffect(() => {
     if (!loggedInUser.loading) {
@@ -44,7 +91,7 @@ const SignupPage = () => {
   }, [loggedInUser, dispatch])
 
   return (
-    <div className='Signup'>
+    <div className='SignupPage'>
       {Object.keys(serverResponse).length > 0 && (
         <Alert
           type={serverResponse.status >= 400 ? 'error' : 'success'}
@@ -75,110 +122,17 @@ const SignupPage = () => {
       <Card title='Sign Up'>
         <Formik
           initialValues={{
-            avatar: undefined,
+            avatar: null,
             userName: '',
             email: '',
             password: '',
             confirmPassword: '',
           }}
-          onSubmit={async (values, { setFieldError }) => {
-            setServerResponse({})
-            try {
-              const data = new FormData()
-              Object.keys(values).forEach(key => data.append(key, values[key]))
-
-              const response = await dispatch(signup(data))
-              console.log(response)
-              const { message } = unwrapResult(response)
-
-              dispatch(
-                createNotification({
-                  id: 'signedup',
-                  type: 'success',
-                  title: 'Your account is successfully created!',
-                  description: message,
-                })
-              )
-            } catch (error) {
-              console.log(error)
-              const { errors, message } = error.data
-              const { status, statusText } = error
-
-              if (errors) {
-                Object.keys(errors).forEach(err => {
-                  if (err === 'userName') {
-                    setExistingUserNames([
-                      ...existingUserNames,
-                      errors[err].value,
-                    ])
-                  }
-                  if (err === 'email') {
-                    setExistingEmails([...existingEmails, errors[err].value])
-                  }
-                  setFieldError(err, errors[err].message)
-                })
-              } else {
-                setServerResponse({
-                  status,
-                  statusText,
-                  message,
-                })
-              }
-            }
-          }}
-          validationSchema={Yup.object().shape({
-            avatar: Yup.mixed()
-              .notRequired()
-              .test(
-                'FILE_TYPE',
-                'Unsuported file selected.\nOnly .PNG or .JPEG (JPG) files are allowed.',
-                value => {
-                  if (value) {
-                    return (
-                      value.type === 'image/png' || value.type === 'image/jpeg'
-                    )
-                  }
-
-                  return true
-                }
-              )
-              .test(
-                'FILE_SIZE',
-                'Chosen file needs to be less then 1MB.',
-                value => {
-                  if (value) {
-                    return value.size <= 1000000
-                  }
-
-                  return true
-                }
-              ),
-            userName: Yup.string()
-              .trim()
-              .min(6, 'Must be at least 6 characters long.')
-              .max(30, 'Maximum 30 characters long.')
-              .required('Please input your username.')
-              .notOneOf(
-                existingUserNames,
-                'Username already taken, please try another one.'
-              ),
-            email: Yup.string()
-              .trim()
-              .email('Invalid email.')
-              .required('Please input your email address.')
-              .notOneOf(
-                existingEmails,
-                'Email already taken, please try another one.'
-              ),
-            // TODO: add regex for password
-            password: Yup.string()
-              .min(6, 'Must be at least 6 characters long.')
-              .required('Please input your password.'),
-            confirmPassword: Yup.string()
-              .min(6, 'Must be at least 6 characters long.')
-              .oneOf([Yup.ref('password')], 'Password needs to be the same.')
-              .required('Please confirm your password'),
-          })}>
+          onSubmit={handleSubmit}
+          validationSchema={signupValidationSchema(
+            existingUserNames,
+            existingEmails
+          )}>
           {({ dirty, isValid }) => (
             <Form layout='vertical' size='large'>
               <Field name='avatar' component={AvatarUpload} />
--- packages/client/src/validation/user-validation.js
+++ packages/client/src/validation/user-validation.js
@@ -1,6 +1,27 @@
 /* eslint-disable import/prefer-default-export */
 import * as Yup from 'yup'
 
+const avatarValidation = Yup.mixed()
+  .notRequired()
+  .test(
+    'FILE_TYPE',
+    'Unsuported file selected.\nOnly .PNG or .JPEG (JPG) files are allowed.',
+    value => {
+      if (value && value.type) {
+        return value.type === 'image/png' || value.type === 'image/jpeg'
+      }
+
+      return true
+    }
+  )
+  .test('FILE_SIZE', 'Chosen file needs to be less then 1MB.', value => {
+    if (value && value.size) {
+      return value.size <= 1000000
+    }
+
+    return true
+  })
+
 const userNameValidation = (
   existingUserNames = [],
   errMsg = '',
@@ -16,11 +37,40 @@ const userNameValidation = (
       errMsg || 'Username already taken, please try another one.'
     )
 
+const emailValidation = (existingEmails = []) =>
+  Yup.string()
+    .trim()
+    .email('Invalid email address.')
+    .required('Please input your email address.')
+    .notOneOf(existingEmails, 'Email already taken, please try another one.')
+
 // TODO: add regex for password
 const passwordValidation = (resetPass = false) =>
   Yup.string()
     .min(6, 'Must be at least 6 characters long.')
     .required(`Please input your ${resetPass ? 'new ' : ''}password.`)
+
+const confirmPasswordValidation = (passRef = 'password', resetPass = false) =>
+  Yup.string()
+    .min(6, 'Must be at least 6 characters long.')
+    .oneOf([Yup.ref(passRef)], 'Password needs to be the same.')
+    .required(`Please confirm your ${resetPass ? 'new ' : ''}password.`)
+
+const isAdminValidation = Yup.bool().required('Is Admin is required field.')
+
+export const signupValidationSchema = (
+  existingUserNames,
+  existingEmails,
+  addNewUser = false
+) =>
+  Yup.object().shape({
+    avatar: avatarValidation,
+    userName: userNameValidation(existingUserNames),
+    email: emailValidation(existingEmails),
+    password: passwordValidation(),
+    confirmPassword: confirmPasswordValidation(),
+    isAdmin: addNewUser ? isAdminValidation : undefined,
+  })
 
 export const signinValidationSchema = (nonExistingUsers, errMsg) =>
   Yup.object().shape({

5d140b106f249c44e9279a5c8b09fcf7a7daff9f
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2024-07-05 00:29:54 +0200

    Refactor the UserProfilePage redux action dispatching and error handling logic and add a cleanup function to cancel already running requests

--- packages/client/src/pages/UserProfilePage/index.jsx
+++ packages/client/src/pages/UserProfilePage/index.jsx
@@ -1,8 +1,15 @@
-/* eslint-disable no-unused-vars */
-import React, { lazy, useEffect, useState, useCallback, Suspense } from 'react'
+import React, {
+  lazy,
+  useEffect,
+  useState,
+  useRef,
+  useCallback,
+  Suspense,
+} from 'react'
 import PropTypes from 'prop-types'
 import { useParams, generatePath } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
+import { useErrorBoundary } from 'react-error-boundary'
 import {
   Row,
   Col,
@@ -32,6 +39,9 @@ import { create as createNotification } from '@redux/notification/notificationSl
 
 import { updateUserValidationSchema } from '@validation/user-validation'
 
+import ErrorHandler from '@utils/ErrorHandler'
+import { handleAsyncThunkError } from '@utils/asyncThunkErrorHandler'
+
 import '../UpdateUserPage/styles.scss'
 
 const PurchasedProductsPage = lazy(() =>
@@ -55,8 +65,23 @@ const UserProfilePage = ({ history, match }) => {
   const [shouldFormReset, setShouldFormReset] = useState(true)
   const [existingUserNames, setExistingUserNames] = useState([])
   const [existingEmails, setExistingEmails] = useState([])
+  const updateUserRef = useRef()
+  const deleteUserRef = useRef()
   const dispatch = useDispatch()
   const { loading, updating, info: user } = useSelector(selectLoggedInUser)
+  const { showBoundary } = useErrorBoundary()
+
+  useEffect(() => {
+    return () => {
+      updateUserRef.current?.abort?.(
+        'Request Aborted due to component unmount.'
+      )
+      deleteUserRef.current?.abort?.(
+        'Request Aborted due to component unmount.'
+      )
+      updatingMessage.destroy('updating')
+    }
+  }, [])
 
   useEffect(() => {
     if (activeTab) {
@@ -76,7 +101,7 @@ const UserProfilePage = ({ history, match }) => {
             path: user.avatarUrl,
             thumbUrl:
               (user.avatarUrl &&
-                `${process.env.REACT_APP_API_BASE_URL}${user.avatarUrl}`) ||
+                `${process.env.REACT_APP_API_BASE_URL + user.avatarUrl}`) ||
               user.cloudinaryUrl ||
               user.photo,
           },
@@ -112,13 +137,15 @@ const UserProfilePage = ({ history, match }) => {
           }
         })
 
-        const { status, statusText, message } = await dispatch(
+        updateUserRef.current = dispatch(
           updateUser({
             uid: user.id,
             updatedData: data,
             updateFor: 'loggedInUser',
           })
-        ).unwrap()
+        )
+
+        await updateUserRef.current?.unwrap?.()
 
         dispatch(
           createNotification({
@@ -132,42 +159,62 @@ const UserProfilePage = ({ history, match }) => {
         setShouldFormReset(true)
         setActiveTabKey('settings')
       } catch (error) {
-        const {
-          status,
-          statusText,
-          data: { errors, message },
-        } = error
+        if (!error.name || error.name !== 'AbortError') {
+          const {
+            status,
+            statusText,
+            data: { errors, message } = {
+              errors: undefined,
+              message: undefined,
+            },
+          } = error
 
-        dispatch(
-          createNotification({
-            id: 'updateUserError',
-            type: 'error',
-            title: `Error, ${statusText}`,
-            description:
-              message ||
-              'Something went wrong while updating a user account, please try again later.',
-          })
-        )
+          if (status === 500) {
+            const boundaryError = new ErrorHandler(
+              message,
+              status,
+              statusText,
+              {
+                redirect: {
+                  to: user?.isAdmin ? '/admin/users' : '/',
+                  text: `Back to ${user?.isAdmin ? 'Users' : 'Home'}`,
+                },
+              }
+            )
 
-        if (errors) {
-          Object.keys(errors).forEach(err => {
-            if (err === 'userName') {
-              setExistingUserNames(userNames => [
-                ...userNames,
-                errors[err].value,
-              ])
-            }
+            showBoundary(boundaryError)
+            return
+          }
 
-            if (err === 'email') {
-              setExistingEmails(emails => [...emails, errors[err].value])
-            }
+          dispatch(
+            createNotification({
+              id: 'updateUserError',
+              type: 'error',
+              title: `Error, ${statusText}`,
+              description:
+                message ||
+                'Something went wrong while updating a user account, please try again later.',
+            })
+          )
 
-            setFieldError(err, errors[err].message)
-          })
+          if (errors) {
+            Object.keys(errors).forEach(err => {
+              if (err === 'userName') {
+                setExistingUserNames(userNames => [
+                  ...userNames,
+                  errors[err].value,
+                ])
+              }
+              if (err === 'email') {
+                setExistingEmails(emails => [...emails, errors[err].value])
+              }
+              setFieldError(err, errors[err].message)
+            })
+          }
         }
       }
     },
-    [dispatch, user.id]
+    [dispatch, user.id, user.isAdmin, showBoundary]
   )
 
   return (
@@ -264,9 +311,13 @@ const UserProfilePage = ({ history, match }) => {
                             'Are you sure that you want to delete your account?',
                           onOk: async () => {
                             try {
-                              const { message } = await dispatch(
+                              deleteUserRef.current = dispatch(
                                 deleteUser(user.id)
-                              ).unwrap()
+                              )
+
+                              const {
+                                message,
+                              } = await deleteUserRef.current?.unwrap?.()
 
                               dispatch(
                                 createNotification({
@@ -278,7 +329,16 @@ const UserProfilePage = ({ history, match }) => {
                               )
 
                               history.push('/')
-                            } catch (error) {
+                            } catch (err) {
+                              handleAsyncThunkError(err, showBoundary, {
+                                redirect: {
+                                  to: user?.isAdmin ? '/admin/users' : '/',
+                                  text: `Back to ${
+                                    user?.isAdmin ? 'Users' : 'Home'
+                                  }`,
+                                },
+                              })
+
                               dispatch(
                                 createNotification({
                                   id: 'deleteUserError',

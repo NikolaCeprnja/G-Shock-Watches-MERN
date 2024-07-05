49254f1ee056d3a30cdfa28b5f939ed3cf60069c
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2024-07-05 22:25:49 +0200

    Refactor the collection asyncThunk payloadCreators to use an AbortController's signal from the thunkAPI that listens for an abort event that'll cancel a costly running Axios request

--- packages/client/src/redux/collection/collectionThunk.js
+++ packages/client/src/redux/collection/collectionThunk.js
@@ -1,25 +1,30 @@
+import axios from 'axios'
 import { createAsyncThunk } from '@reduxjs/toolkit'
 
 import collections from '@api/collection/index'
+import asyncThunkErrorHandler from '@utils/asyncThunkErrorHandler'
 
-// Thunks
 export const getCollections = createAsyncThunk(
   'collections/getCollections',
-  async (arg, { rejectWithValue }) => {
+  async (arg, { signal, rejectWithValue }) => {
     try {
-      const response = await collections.getCollections()
+      const source = axios.CancelToken.source()
+      signal.addEventListener('abort', () => {
+        source.cancel('Request Aborted due to component unmount.')
+      })
+
+      const response = await collections.getCollections(source.token)
       const { data, status } = response
       return { ...data, status }
     } catch (err) {
-      const { data, status, statusText } = err.response
-      return rejectWithValue({ data, status, statusText })
+      return asyncThunkErrorHandler(err, rejectWithValue)
     }
   }
 )
 
 export const getCollectionsByGender = createAsyncThunk(
   'collections/getCollectionByGender',
-  async (arg, { rejectWithValue }) => {
+  async (arg, { signal, rejectWithValue }) => {
     let gender
 
     if (typeof arg === 'string') {
@@ -29,12 +34,19 @@ export const getCollectionsByGender = createAsyncThunk(
     }
 
     try {
-      const response = await collections.getCollectionsByGender(gender)
+      const source = axios.CancelToken.source()
+      signal.addEventListener('abort', () => {
+        source.cancel('Request Aborted due to component unmount.')
+      })
+
+      const response = await collections.getCollectionsByGender(
+        gender,
+        source.token
+      )
       const { data, status } = response
       return { ...data, status }
     } catch (err) {
-      const { data, status, statusText } = err.response
-      return rejectWithValue({ data, status, statusText })
+      return asyncThunkErrorHandler(err, rejectWithValue)
     }
   }
 )

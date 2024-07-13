bc3343003892b3cf4657f09b4a4d4b00355d1b4c
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2024-07-13 23:10:52 +0200

    Refactor the product asyncThunk payloadCreators to use an AbortController's signal from the thunkAPI that listens for an abort event that'll cancel a costly running Axios request

--- packages/client/src/redux/product/productThunk.js
+++ packages/client/src/redux/product/productThunk.js
@@ -1,73 +1,99 @@
+import axios from 'axios'
 import { createAsyncThunk } from '@reduxjs/toolkit'
 
 import products from '@api/product/index'
+import asyncThunkErrorHandler from '@utils/asyncThunkErrorHandler'
 
 export const getProducts = createAsyncThunk(
   'products/getProducts',
-  async (urlQueryParams, { rejectWithValue }) => {
+  async (params, { signal, rejectWithValue }) => {
     try {
-      const response = await products.getProducts(urlQueryParams)
+      const source = axios.CancelToken.source()
+      signal.addEventListener('abort', () => {
+        source.cancel('Request Aborted due to component unmount.')
+      })
+
+      const response = await products.getProducts(params, source.token)
       const { data, status } = response
       return { ...data, status }
     } catch (err) {
-      const { data, status, statusText } = err.response
-      return rejectWithValue({ data, status, statusText })
+      return asyncThunkErrorHandler(err, rejectWithValue)
     }
   }
 )
 
 export const getLatestProducts = createAsyncThunk(
   'products/getLatestProducts',
-  async () => {
+  async (arg, { signal, rejectWithValue }) => {
     try {
-      const response = await products.getLatestProducts()
+      const source = axios.CancelToken.source()
+      signal.addEventListener('abort', () => {
+        source.cancel('Request Aborted due to component unmount.')
+      })
+
+      const response = await products.getLatestProducts(source.token)
       const { data, status } = response
       return { ...data, status }
     } catch (err) {
-      return err.response
+      return asyncThunkErrorHandler(err, rejectWithValue)
     }
   }
 )
 
 export const getTopRatedProducts = createAsyncThunk(
   'products/getTopRatedProducts',
-  async () => {
+  async (arg, { signal, rejectWithValue }) => {
     try {
-      const response = await products.getTopRatedProducts()
+      const source = axios.CancelToken.source()
+      signal.addEventListener('abort', () => {
+        source.cancel('Request Aborted due to component unmount.')
+      })
+
+      const response = await products.getTopRatedProducts(source.token)
       const { data, status } = response
       return { ...data, status }
     } catch (err) {
-      return err.response
+      return asyncThunkErrorHandler(err, rejectWithValue)
     }
   }
 )
 
 export const getProductById = createAsyncThunk(
   'products/getProductById',
-  async (pid, { rejectWithValue }) => {
+  async (pid, { signal, rejectWithValue }) => {
     try {
-      const response = await products.getProductById(pid)
+      const source = axios.CancelToken.source()
+      signal.addEventListener('abort', () => {
+        source.cancel('Request Aborted due to component unmount.')
+      })
+
+      const response = await products.getProductById(pid, source.token)
       const { data, status } = response
       return { ...data, status }
     } catch (err) {
-      const { data, status, statusText } = err.response
-      return rejectWithValue({ data, status, statusText })
+      return asyncThunkErrorHandler(err, rejectWithValue)
     }
   }
 )
 
-// TODO: Add getProductReviews async thunk
-
 export const updateProduct = createAsyncThunk(
   'products/updateProduct',
-  async ({ pid, updatedData }, { rejectWithValue }) => {
+  async ({ pid, updatedData }, { signal, rejectWithValue }) => {
     try {
-      const response = await products.updateProduct(pid, updatedData)
+      const source = axios.CancelToken.source()
+      signal.addEventListener('abort', () => {
+        source.cancel('Request Aborted due to component unmount.')
+      })
+
+      const response = await products.updateProduct(
+        pid,
+        updatedData,
+        source.token
+      )
       const { data, status } = response
       return { ...data, status }
     } catch (err) {
-      const { data, status, statusText } = err.response
-      return rejectWithValue({ data, status, statusText })
+      return asyncThunkErrorHandler(err, rejectWithValue)
     }
   }
 )

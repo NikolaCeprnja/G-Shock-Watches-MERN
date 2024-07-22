e12f70cbe19d904c89bc951b40a90daec3ecd9bc
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2024-07-22 20:31:59 +0200

    Refactor the StatisticCard's async data fetching and error handling to use the Axios CancelToken API and add a cleanup function to cancel an already running request when component unmounts

--- packages/client/src/containers/StatisticCard/index.jsx
+++ packages/client/src/containers/StatisticCard/index.jsx
@@ -1,27 +1,52 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import PropTypes from 'prop-types'
+import { useErrorBoundary } from 'react-error-boundary'
+
+import ErrorHandler from '@utils/ErrorHandler'
 
 const StatisticCard = ({ request, children, periodFor, wrapperClassName }) => {
   const [data, setData] = useState()
   const [queryParams, setQueryParams] = useState({ period: periodFor })
   const [loading, setLoading] = useState(false)
+  const { showBoundary } = useErrorBoundary()
 
   useEffect(() => {
+    const source = axios.CancelToken.source()
+
     const fetchData = async () => {
       try {
         setLoading(true)
         const {
           data: { message, ...value },
-        } = await request(queryParams)
+        } = await request(source.token, queryParams)
         setData(value)
         setLoading(false)
       } catch (err) {
-        setLoading(false)
+        if (!axios.isCancel(err) && !err.isAxiosError) {
+          const {
+            status,
+            statusText,
+            data: { error, message },
+          } = err.response
+
+          setLoading(false)
+
+          const boundaryError = new ErrorHandler(message, status, statusText, {
+            cause: error,
+          })
+
+          showBoundary(boundaryError)
+        }
       }
     }
 
     fetchData()
-  }, [request, queryParams])
+
+    return () => {
+      source.cancel('Request Aborted due to component unmount.')
+    }
+  }, [request, queryParams, showBoundary])
 
   return (
     <div className={wrapperClassName}>

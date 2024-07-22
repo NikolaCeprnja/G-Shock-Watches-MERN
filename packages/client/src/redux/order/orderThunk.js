0c19997e78f7e463a7838d6015a15670fec850eb
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2024-07-22 22:08:37 +0200

    Refactor the order asyncThunk payloadCreators to use an AbortController's signal from the thunkAPI that listens for an abort event that'll cancel a costly running Axios request

--- packages/client/src/redux/order/orderThunk.js
+++ packages/client/src/redux/order/orderThunk.js
@@ -1,32 +1,41 @@
-/* eslint-disable no-unused-vars */
+import axios from 'axios'
 import { createAsyncThunk } from '@reduxjs/toolkit'
 
 import orders from '@api/order/index'
+import asyncThunkErrorHandler from '@utils/asyncThunkErrorHandler'
 
 export const getOrders = createAsyncThunk(
   'orders/getOrders',
-  async (urlQueryParams, { rejectWithValue }) => {
+  async (params, { signal, rejectWithValue }) => {
     try {
-      const response = await orders.getOrders(urlQueryParams)
+      const source = axios.CancelToken.source()
+      signal.addEventListener('abort', () => {
+        source.cancel('Request Aborted due to component unmount.')
+      })
+
+      const response = await orders.getOrders(params, source.token)
       const { data, status } = response
       return { ...data, status }
     } catch (err) {
-      const { data, status, statusText } = err.response
-      return rejectWithValue({ data, status, statusText })
+      return asyncThunkErrorHandler(err, rejectWithValue)
     }
   }
 )
 
 export const getOrderById = createAsyncThunk(
   'orders/getOrderById',
-  async (oid, { rejectWithValue }) => {
+  async (oid, { signal, rejectWithValue }) => {
     try {
-      const response = await orders.getOrderById(oid)
+      const source = axios.CancelToken.source()
+      signal.addEventListener('abort', () => {
+        source.cancel('Request Aborted due to component unmount.')
+      })
+
+      const response = await orders.getOrderById(oid, source.token)
       const { data, status } = response
       return { ...data, status }
     } catch (err) {
-      const { data, status, statusText } = err.response
-      return rejectWithValue({ data, status, statusText })
+      return asyncThunkErrorHandler(err, rejectWithValue)
     }
   }
 )

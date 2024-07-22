63a5925a66dba0e2824d90b09049c05dab687efe
Nikola Ceprnja <nikolaceprnja997@gmail.com>
2024-07-22 23:53:32 +0200

    Refactor the offer asyncThunk payloadCreator to use an AbortController's signal from the thunkAPI that listens for an abort event that'll cancel a costly running Axios request

--- packages/client/src/redux/offer/offerThunk.js
+++ packages/client/src/redux/offer/offerThunk.js
@@ -1,18 +1,24 @@
 /* eslint-disable import/prefer-default-export */
+import axios from 'axios'
 import { createAsyncThunk } from '@reduxjs/toolkit'
 
 import offers from '@api/offer/index'
+import asyncThunkErrorHandler from '@utils/asyncThunkErrorHandler'
 
 export const getOffers = createAsyncThunk(
   'offers/getOffers',
-  async (offer, { rejectWithValue }) => {
+  async (arg, { signal, rejectWithValue }) => {
     try {
-      const response = await offers.getOffers()
+      const source = axios.CancelToken.source()
+      signal.addEventListener('abort', () => {
+        source.cancel('Request Aborted due to component unmount.')
+      })
+
+      const response = await offers.getOffers(source.token)
       const { data, status } = response
       return { ...data, status }
     } catch (err) {
-      const { data, status, statusText } = err.response
-      return rejectWithValue({ data, status, statusText })
+      return asyncThunkErrorHandler(err, rejectWithValue)
     }
   }
 )
